aa8bd682e45049375797a046f7e3b163ce2afeee
Adrian Rumpold <a.rumpold@gmail.com>
2024-04-05 11:00:39 +0200

    feat: Implement Kueue Kubernetes Runner
    Issue: #1

--- example_1.py
+++ example_1.py
@@ -1,12 +1,64 @@
-from jobs import ImageBuilder, Runner, job
+import argparse
+import enum
+import logging
+
+from jobs import ImageBuilder, job
+from jobs.runner import DockerRunner, KueueRunner
 
 
 @job
-def my_job() -> None:
+def myjob() -> None:
     print("Hello, world")
 
 
+class ExecutionMode(enum.StrEnum):
+    LOCAL = enum.auto()
+    DOCKER = enum.auto()
+    KUEUE = enum.auto()
+
+
+def _make_parser():
+    parser = argparse.ArgumentParser(
+        description="Run an example job either locally, or on a container execution platform",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--image-name",
+        help="Image name to use when building a container image",
+        default="example:latest",
+    )
+
+    parser.add_argument(
+        "--mode",
+        help="Job execution mode",
+        default="local",
+        choices=[val for val in ExecutionMode],
+        type=ExecutionMode,
+    )
+
+    return parser
+
+
 if __name__ == "__main__":
-    # image = ImageBuilder.from_dockerfile("example:latest")
-    # Runner.run(my_job, image)
-    my_job()
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger("urllib3.connectionpool").setLevel(logging.INFO)
+
+    argparser = _make_parser()
+    args, _ = argparser.parse_known_args()
+
+    mode = args.mode
+    logging.debug(f"Execution mode: {mode}")
+
+    if mode != ExecutionMode.LOCAL:
+        image = ImageBuilder.from_dockerfile(args.image_name)
+
+    if mode == ExecutionMode.DOCKER:
+        # Submit the job as a container
+        DockerRunner.run(myjob, image)
+    elif mode == ExecutionMode.KUEUE:
+        # Submit the job as a Kueue Kubernetes Job
+        KueueRunner.run(myjob, image)
+    elif mode == ExecutionMode.LOCAL:
+        # Run the job locally
+        myjob()
--- src/jobs/runner.py
+++ src/jobs/runner.py
@@ -1,21 +1,89 @@
+import abc
+import logging
+
 import docker
+from kubernetes import client, config
 
 from jobs import Image, Job
 
 JOBS_EXECUTE_CMD = "jobs_execute"
+KUEUE_DEFAULT_NAMESPACE = "default"
 
 
-class Runner:
+def _make_command(job: Job) -> list[str]:
+    return [
+        JOBS_EXECUTE_CMD,
+        job._file,
+        job._name,
+    ]
+
+
+class Runner(abc.ABC):
+    @staticmethod
+    @abc.abstractmethod
+    def run(job: Job, image: Image) -> None: ...
+
+
+class DockerRunner(Runner):
     @staticmethod
     def run(job: Job, image: Image) -> None:
-        command = [
-            JOBS_EXECUTE_CMD,
-            job._file,
-            job._name,
-        ]
+        command = _make_command(job)
 
         client = docker.from_env()
         client.containers.run(
-            image = image.tag,
-            command = command,
+            image=image.tag,
+            command=command,
         )
+
+
+class KueueRunner(Runner):
+    @staticmethod
+    def _make_job_crd(job: Job, image: Image) -> client.V1Job:
+        # FIXME: Name needs to be RFC1123-compliant, add validation/sanitation
+        metadata = client.V1ObjectMeta(
+            generate_name=job._name,
+            labels={"kueue.x-k8s.io/queue-name": "user-queue"},
+        )
+
+        # Job container
+        container = client.V1Container(
+            image=image.tag,
+            image_pull_policy="IfNotPresent",
+            name="dummy-job",
+            command=_make_command(job),
+            resources={
+                "requests": {
+                    "cpu": 1,
+                    "memory": "200Mi",
+                }
+            },
+        )
+
+        # Job template
+        template = {
+            "spec": {
+                "containers": [container],
+                "restartPolicy": "Never",
+            }
+        }
+        return client.V1Job(
+            api_version="batch/v1",
+            kind="Job",
+            metadata=metadata,
+            spec=client.V1JobSpec(
+                parallelism=1,
+                completions=1,
+                suspend=True,
+                template=template,
+            ),
+        )
+
+    @staticmethod
+    def run(job: Job, image: Image) -> None:
+        logging.info(f"Submitting job {job._name} to Kueue")
+
+        config.load_kube_config()
+        k8s_job = KueueRunner._make_job_crd(job, image)
+        batch_api = client.BatchV1Api()
+        batch_api.create_namespaced_job(KUEUE_DEFAULT_NAMESPACE, k8s_job)
+        logging.info("Submitted job successfully to Kueue.")
