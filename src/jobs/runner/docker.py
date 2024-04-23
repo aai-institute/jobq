38dafbe2897e9be1d58e50675c5fa609d3741eda
Adrian Rumpold <a.rumpold@gmail.com>
2024-04-23 13:49:57 +0200

    chore: Split up the Runner module
    The `runner.py` module was growing to a point where readability was hampered.
    
    This commit splits it up into the `jobs.runner` package, with submodules
    grouped by execution platform.
    
    Also, introduces a `KubernetesNamespaceMixin` to simplify determining the
    current or desired namespace for a workload.

---
+++ src/jobs/runner/docker.py
@@ -0,0 +1,39 @@
+import logging
+import textwrap
+
+import docker
+
+from jobs import Image, Job
+from jobs.job import DockerResourceOptions
+from jobs.runner.base import Runner, _make_executor_command
+from jobs.util import remove_none_values
+
+
+class DockerRunner(Runner):
+    def __init__(self):
+        self._client = docker.from_env()
+
+    def run(self, job: Job, image: Image) -> None:
+        command = _make_executor_command(job)
+
+        resource_kwargs: DockerResourceOptions = {
+            "mem_limit": None,
+            "nano_cpus": None,
+            "device_requests": None,
+        }
+        if job.options and (res := job.options.resources):
+            resource_kwargs = res.to_docker()
+
+        container: docker.api.client.ContainerApiMixin = self._client.containers.run(
+            image=image.tag,
+            command=command,
+            detach=True,
+            **remove_none_values(resource_kwargs),
+        )
+
+        exit_code = container.wait()
+
+        logging.debug(
+            f"Container exited with code {exit_code.get('StatusCode')}, output:\n%s",
+            textwrap.indent(container.logs().decode(encoding="utf-8"), " " * 4),
+        )
---
+++ src/jobs/runner/ray.py
@@ -0,0 +1,115 @@
+import logging
+import random
+import shlex
+import string
+import sys
+import time
+from pathlib import Path
+
+import yaml
+from kubernetes import client
+from ray.dashboard.modules.job.common import JobStatus
+from ray.dashboard.modules.job.sdk import JobSubmissionClient
+from typing_extensions import deprecated
+
+import jobs
+from jobs import Image, Job
+from jobs.runner.base import Runner, _make_executor_command
+from jobs.util import KubernetesNamespaceMixin
+
+
+class RayClusterRunner(Runner, KubernetesNamespaceMixin):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+        self._head_url = kwargs.get("head_url")
+        if self._head_url is None:
+            raise ValueError("Ray cluster head URL is unset")
+
+    @deprecated("Needs more ideation")
+    def _create_ray_cluster(self) -> None:
+        """Create a new Ray cluster in Kubernetes using the Kuberay operator."""
+        api = client.CustomObjectsApi()
+
+        manifest = (Path(__file__).parents[2] / "raycluster-manifest.yaml").read_text(
+            "utf-8"
+        )
+        body = yaml.safe_load(manifest)
+        obj = api.create_namespaced_custom_object(
+            "ray.io", "v1", self.namespace, "rayclusters", body
+        )
+
+        logging.debug(f"Created Ray cluster {obj.metadata.name}")
+
+    @deprecated("Needs more ideation")
+    def _get_cluster(self):
+        """Return the head URL of a Ray cluster running in Kubernetes."""
+        api = client.CustomObjectsApi()
+        status = api.get_namespaced_custom_object_status(
+            "ray.io", "v1", self.namespace, "rayclusters", "raycluster-kuberay"
+        )
+        if status is None:
+            return None
+        return status
+
+    @staticmethod
+    def _wait_until_status(
+        job_id,
+        job_client: JobSubmissionClient,
+        status_to_wait_for=frozenset(
+            {JobStatus.SUCCEEDED, JobStatus.STOPPED, JobStatus.FAILED}
+        ),
+        timeout_seconds=10,
+    ) -> tuple[float, JobStatus]:
+        """Wait until a Ray Job has entered any of a set of desired states (defaults to all final states)."""
+
+        start = time.time()
+        status = job_client.get_job_status(job_id)
+
+        # 0 means no timeout
+        if timeout_seconds == 0:
+            timeout_seconds = sys.maxsize
+
+        while time.time() - start <= timeout_seconds:
+            if status in status_to_wait_for:
+                break
+            time.sleep(0.5)
+            status = job_client.get_job_status(job_id)
+
+        return time.time() - start, status
+
+    def run(self, job: Job, image: Image) -> None:
+        # TODO: This is non-functional - need to consider split between user and IT ops
+        # if self._head_url is None:
+        #     cluster = self._get_cluster()
+        #     if cluster is None:
+        #         self._create_ray_cluster()
+        #         head_url = ""
+        #     else:
+        #         head_url = cluster.get("head", {}).get("serviceIP")
+
+        head_url = self._head_url
+        logging.info(f"Submitting job {job.name} to Ray cluster at {head_url!r}")
+
+        ray_jobs = JobSubmissionClient(head_url)
+
+        # TODO: Lots of hardcoded stuff here
+        suffix = "".join(random.choices(string.ascii_lowercase + string.digits, k=4))
+        job_id = ray_jobs.submit_job(
+            submission_id=f"{job.name}_{suffix}",
+            entrypoint=shlex.join(_make_executor_command(job)),
+            runtime_env={
+                "working_dir": Path(job.file).parent,
+                "pip": Path("requirements.txt").read_text("utf-8").splitlines(),
+                "py_modules": [jobs],
+            },
+            **(res.to_ray() if job.options and (res := job.options.resources) else {}),
+        )
+        logging.info(f"Submitted Ray job with ID {job_id}")
+
+        execution_time, status = self._wait_until_status(
+            job_id, ray_jobs, timeout_seconds=0
+        )
+        logging.info(
+            f"Job finished with status {status.value!r} in {execution_time:.1f}s"
+        )
