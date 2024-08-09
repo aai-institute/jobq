339d3d97943f854c13ae04119714e30133df84af
Adrian Rumpold <a.rumpold@gmail.com>
2024-08-09 12:04:19 +0200

    fix(backend): Mock Kubernetes config loading in integration tests

--- backend/tests/integration/conftest.py
+++ backend/tests/integration/conftest.py
@@ -1,9 +1,12 @@
+import kubernetes.config
 import pytest
 from fastapi.testclient import TestClient
+from pytest_mock import MockFixture
 
 from jobs_server import app
 
 
-@pytest.fixture(scope="session")
-def client() -> TestClient:
+@pytest.fixture
+def client(mocker: MockFixture) -> TestClient:
+    mocker.patch.object(kubernetes.config, "load_config")
     return TestClient(app)
