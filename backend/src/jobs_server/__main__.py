477c51a27ee81f017e46c48598605399f0024280
Max Mynter <maxmynter@me.com>
2024-08-12 13:53:49 +0200

    Add Job status API to backend

--- backend/src/jobs_server/__main__.py
+++ backend/src/jobs_server/__main__.py
@@ -1,14 +1,31 @@
 import logging
 from contextlib import asynccontextmanager
+from datetime import datetime
+from enum import Enum
 
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
-from jobs import Image, Job
-from kubernetes import client, config
-
 from jobs_server.models import CreateJobModel
 from jobs_server.runner import Runner
 from jobs_server.runner.base import ExecutionMode
+from kubernetes import client, config
+from pydantic import BaseModel
+
+from jobs import Image, Job
+
+
+class JobStatus(Enum):
+    PENDING = "pending"
+    EXECUTING = "executing"
+    SUCCEEDED = "succeeded"
+    FAILED = "failed"
+
+
+class JobStatusResponse(BaseModel):
+    status: JobStatus
+    submission_time: datetime | None
+    start_time: datetime | None
+    completion_time: datetime | None
 
 
 @asynccontextmanager
@@ -51,6 +68,38 @@ async def submit_job(opts: CreateJobModel):
 
     job_uid = runner.run(job, image)
     return job_uid
+
+
+@app.get("/jobs/{job_status}/status", response_model=JobStatusResponse)
+async def get_job_status(job_id: str, namespace: str = "default"):
+    try:
+        api = client.BatchV1Api()
+        job = api.read_namespaced_job(name=job_id, namespace=namespace)
+
+        if job.status.active:
+            status = JobStatus.EXECUTING
+        elif job.status.succeeded:
+            status = JobStatus.SUCCEEDED
+        elif job.status.failed:
+            status = JobStatus.FAILED
+        else:
+            status = JobStatus.PENDING
+
+        submission_time = job.metadata.creation_timestamp
+        start_time = job.status.start_time
+        completion_time = job.status.completion_time
+
+        return JobStatusResponse(
+            status=status,
+            submission_time=submission_time,
+            start_time=start_time,
+            completion_time=completion_time,
+        )
+    except client.ApiException as e:
+        if e.status == 404:
+            raise HTTPException(status_code=404, detail=f"Job not found: {job_id}")
+        else:
+            raise HTTPException(status_code=500, details=f"Kubernetes Error: {e}")
 
 
 def generate_logs(pod: client.V1Pod):
