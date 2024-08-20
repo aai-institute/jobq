855569bc0b2215a66002ce37594832b507a62adc
Adrian Rumpold <a.rumpold@gmail.com>
2024-08-20 15:17:07 +0200

    fix(infra): GAR permissions

--- infra/index.ts
+++ infra/index.ts
@@ -9,92 +9,98 @@ const zone = gcp_config.require("zone");
 
 // VPC network
 const network = gcp.compute.getNetwork({
-    name: "infra-product-dev",
+  name: "infra-product-dev",
 });
 const subnet = "compute";
 
 // VM for Dagster
 const sa = new gcp.serviceaccount.Account("playground", {
-    accountId: "playground",
-    displayName: "Service account for Playground VM",
+  accountId: "playground",
+  displayName: "Service account for Playground VM",
 });
 // Application roles for SA, including Ops Agent (Logs & Metric Writer)
 for (const role of [
-    "roles/logging.logWriter",
-    "roles/monitoring.metricWriter",
+  "roles/logging.logWriter",
+  "roles/monitoring.metricWriter",
+  "roles/artifactregistry.admin",
 ]) {
-    new gcp.projects.IAMMember(`playground-${role}`, {
-        project,
-        member: pulumi.concat("serviceAccount:", sa.email),
-        role,
-    });
+  new gcp.projects.IAMMember(`playground-${role}`, {
+    project,
+    member: pulumi.concat("serviceAccount:", sa.email),
+    role,
+  });
 }
 
 const instanceTemplate = new gcp.compute.InstanceTemplate("infra-product-dev", {
-    region,
-    machineType: "e2-standard-8",
-    disks: [
+  region,
+  machineType: "e2-standard-8",
+  disks: [
+    {
+      boot: true,
+      sourceImage: "debian-cloud/debian-12",
+      diskSizeGb: 64,
+      diskType: "pd-balanced",
+    },
+  ],
+  metadataStartupScript: fs.readFileSync("./vm-startup.sh", "utf-8"),
+  networkInterfaces: [
+    {
+      network: network.then((n) => n.id),
+      subnetwork: subnet,
+      accessConfigs: [
         {
-            boot: true,
-            sourceImage: "debian-cloud/debian-12",
-            diskSizeGb: 64,
-            diskType: "pd-balanced",
+          networkTier: "PREMIUM",
         },
-    ],
-    metadataStartupScript: fs.readFileSync("./vm-startup.sh", "utf-8"),
-    networkInterfaces: [
-        {
-            network: network.then((n) => n.id),
-            subnetwork: subnet,
-            accessConfigs: [
-                {
-                    networkTier: "PREMIUM",
-                },
-            ],
-        },
-    ],
-    tags: ["ssh"],
-    labels: {
-        environment: "playground",
+      ],
     },
-    serviceAccount: {
-        email: sa.email,
-        scopes: ["https://www.googleapis.com/auth/cloud-platform"],
-    },
-    scheduling: {
-        preemptible: true,
-        automaticRestart: false, // required for preemptible instances
-    },
+  ],
+  tags: ["ssh"],
+  labels: {
+    environment: "playground",
+  },
+  serviceAccount: {
+    email: sa.email,
+    scopes: ["https://www.googleapis.com/auth/cloud-platform"],
+  },
+  scheduling: {
+    preemptible: true,
+    automaticRestart: false, // required for preemptible instances
+  },
 });
 
 new gcp.compute.InstanceGroupManager("playground", {
-    zone,
-    versions: [
-        {
-            instanceTemplate: instanceTemplate.id,
-        },
-    ],
+  zone,
+  versions: [
+    {
+      instanceTemplate: instanceTemplate.id,
+    },
+  ],
 
-    baseInstanceName: "infra-product-dev",
-    targetSize: 1,
+  baseInstanceName: "infra-product-dev",
+  targetSize: 1,
 });
 
 new gcp.compute.Firewall("inbound-ssh-from-iap", {
-    network: network.then((n) => n.id),
-    sourceRanges: ["35.235.240.0/20"],
-    targetServiceAccounts: [sa.email],
-    allows: [
-        {
-            protocol: "TCP",
-            ports: ["22"],
-        },
-    ],
-    direction: "INGRESS",
+  network: network.then((n) => n.id),
+  sourceRanges: ["35.235.240.0/20"],
+  targetServiceAccounts: [sa.email],
+  allows: [
+    {
+      protocol: "TCP",
+      ports: ["22"],
+    },
+  ],
+  direction: "INGRESS",
 });
 
 // Artifact Registry
-new gcp.artifactregistry.Repository("dev", {
-    format: "Docker",
-    repositoryId: "infra-product-dev",
-    location: region,
+const repo = new gcp.artifactregistry.Repository("dev", {
+  format: "Docker",
+  repositoryId: "infra-product-dev",
+  location: region,
+});
+new gcp.artifactregistry.RepositoryIamMember("public-pull", {
+  repository: repo.id,
+  member: "allUsers",
+  role: "roles/artifactregistry.reader",
 });
