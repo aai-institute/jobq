7f641699d97e271fc699ffff51ac2cf3eca0b2bf
Adrian Rumpold <a.rumpold@gmail.com>
2024-08-22 10:38:19 +0200

    fix(backend): Fix lookup of current namespace in-cluster

--- backend/src/jobs_server/services/k8s.py
+++ backend/src/jobs_server/services/k8s.py
@@ -1,10 +1,12 @@
 import logging
 from collections.abc import Generator
+from pathlib import Path
 
 from kubernetes import client, config
 
 from jobs_server.exceptions import PodNotReadyError, WorkloadNotFound
 from jobs_server.models import JobId
+from jobs_server.utils.helpers import traverse
 from jobs_server.utils.kueue import KueueWorkload
 
 
@@ -12,19 +14,32 @@ class KubernetesService:
     def __init__(self):
         try:
             config.load_incluster_config()
+            self._in_cluster = True
         except config.ConfigException:
             logging.warning(
                 "Could not load in-cluster config, attempting to load Kubeconfig",
                 exc_info=True,
             )
             config.load_kube_config()
+            self._in_cluster = False
 
         self._core_v1_api = client.CoreV1Api()
 
     @property
     def namespace(self) -> str:
-        _, active_context = config.list_kube_config_contexts()
-        current_namespace = active_context["context"].get("namespace")
+        if not self._in_cluster:
+            _, active_context = config.list_kube_config_contexts()
+            current_namespace = traverse(active_context, "context.namespace")
+        else:
+            # When running in a cluster, determine the namespace from the mounted service account
+            try:
+                current_namespace = (
+                    Path("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+                    .read_text()
+                    .strip()
+                )
+            except FileNotFoundError as e:
+                raise RuntimeError("Could not determine current namespace") from e
         return current_namespace
 
     def workload_for_managed_resource(
