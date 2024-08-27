f2032adae17f19758df6303a5d00b2a5331941ac
Adrian Rumpold <a.rumpold@gmail.com>
2024-08-27 08:37:27 +0200

    tests(backend): Full workload lifecycle e2e test
    The test exercises the following functionality:
    
    - create a job from options
    - fetch its execution status
    - fetch its logs
    - terminate the workload

--- backend/tests/e2e/test_example.py
+++ backend/tests/e2e/test_example.py
@@ -1,9 +1,12 @@
+import time
+from pprint import pp
+
 from fastapi.encoders import jsonable_encoder
 from fastapi.testclient import TestClient
 from jobs import JobOptions, SchedulingOptions
 from testcontainers.core.image import DockerImage
 
-from jobs_server.models import CreateJobModel, WorkloadIdentifier
+from jobs_server.models import CreateJobModel, JobStatus, WorkloadIdentifier
 
 
 def test_create_job(client: TestClient, job_image: DockerImage):
@@ -21,11 +24,34 @@ def test_create_job(client: TestClient, job_image: DockerImage):
     response = client.post("/jobs", json=jsonable_encoder(body))
     workload_id = WorkloadIdentifier.model_validate_json(response.text)
 
-    # Check workload status
-    response = client.get(f"/jobs/{workload_id.uid}/status")
-    assert response.status_code == 200
-    assert response.json() in ["pending", "executing"]
+    time.sleep(0.5)
 
-    # Check workload logs
-    response = client.get(f"/jobs/{workload_id.uid}/logs")
-    assert response.status_code == 200
+    # Check workload status
+    status: JobStatus = None
+    while True:
+        response = client.get(f"/jobs/{workload_id.uid}/status")
+        assert response.status_code == 200
+
+        status = JobStatus(response.json())
+        assert status != JobStatus.FAILED
+
+        if status != JobStatus.PENDING:
+            break
+
+        time.sleep(1)
+
+    pp(status)
+
+    # Check workload logs (retry if pod is not ready yet)
+    while True:
+        response = client.get(f"/jobs/{workload_id.uid}/logs")
+        assert response.status_code in [200, 400]
+
+        if response.status_code == 200:
+            break
+        elif response.status_code == 400:
+            assert response.json().get("detail") == "pod not ready"
+
+    # Terminate the workload
+    response = client.post(f"/jobs/{workload_id.uid}/stop")
+    assert response.status_code == 204
