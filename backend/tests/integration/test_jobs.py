7af05b49b2e391b1115851ae7622771999c4a1e9
Max Mynter <32773644+maxmynter@users.noreply.github.com>
2024-08-29 11:03:14 +0200

    Return detailed information on Job status endpoint.  (#60)
    * (chore) Rename JobStatus to WorkloadExecutionStatus
    
    for consistent naming convention of Workload and Jobs
    
    * Move K8s and ManagedWorkload def's to dependencies
    
    to clean have less concerns in the job router.
    
    * feat(backend) Add details to Job Status return
    
    * (tests) Update the Tests for the Status Endpoint
    
    * (chore) Move WorkloadMetadata into models.py and fix circ. imports
    
    * (chore) Revert JobStatus name Refactor
    
    from WorkloadExecutionStatus. It is now, again, JobStatus.
    This is, because it is part of the API spec.
    
    * (chore) Revert Name Refactor of status endpoint
    
    because it is part of the API client.
    The initial change was motivated by the shadowing of the `status`
    function and HTTP `status` imports from FastAPI.
    We resolved shadowing via named import as `http_status`.
    
    * tests(backend): Fix regression in e2e test
    
    * chore: Set Codecov flags as  carryforward
    
    This allows us to run partial CI pipelines and still get full coverage
    information in the Codecov UI.
    
    * fix(backend): Refactor workload status model and improve tests
    
    ---------
    
    Co-authored-by: Adrian Rumpold <a.rumpold@gmail.com>

--- backend/tests/integration/test_jobs.py
+++ backend/tests/integration/test_jobs.py
@@ -4,14 +4,26 @@ import pytest
 from fastapi.encoders import jsonable_encoder
 from fastapi.testclient import TestClient
 from jobs import JobOptions
+from kubernetes import client as k8s_client
 from pytest_mock import MockFixture
 
 from jobs_server.exceptions import PodNotReadyError
-from jobs_server.models import CreateJobModel, WorkloadIdentifier
+from jobs_server.models import (
+    CreateJobModel,
+    JobStatus,
+    WorkloadIdentifier,
+    WorkloadMetadata,
+)
 from jobs_server.runner import KueueRunner, RayJobRunner
 from jobs_server.runner.base import ExecutionMode, Runner
 from jobs_server.runner.docker import DockerRunner
 from jobs_server.services.k8s import KubernetesService
+from jobs_server.utils.kueue import (
+    KueueWorkload,
+    WorkloadAdmission,
+    WorkloadSpec,
+    WorkloadStatus,
+)
 
 
 @pytest.mark.parametrize(
@@ -61,21 +73,42 @@ def test_submit_job(
         mock.assert_called_once()
         assert response.is_success
 
-        response_model = WorkloadIdentifier.model_validate(response.json())
+        response_model = WorkloadIdentifier.model_validate_json(response.text)
         assert response_model == job_id
 
 
 class TestJobStatus:
     def test_success(self, client: TestClient, mocker: MockFixture) -> None:
-        mock = mocker.patch.object(KubernetesService, "workload_for_managed_resource")
+        wl = KueueWorkload(
+            owner_uid=uuid.uuid4(),
+            metadata=k8s_client.V1ObjectMeta(uid=uuid.uuid4()),
+            execution_status=JobStatus.EXECUTING,
+            spec=WorkloadSpec(
+                queueName="default",
+                active=True,
+                podSets=[],
+            ),
+            status=WorkloadStatus(
+                conditions=[{"reason": "Admitted", "type": "Admitted", "status": True}],
+                admission=WorkloadAdmission(
+                    clusterQueue="default",
+                    podSetAssignments=[],
+                ),
+            ),
+        )
+        mocker.patch.object(KueueWorkload, "for_managed_resource", return_value=wl)
 
-        job_id = uuid.uuid4()
-        response = client.get(f"/jobs/{job_id}/status")
+        response = client.get(f"/jobs/{wl.metadata.uid}/status")
 
-        assert response.is_success
-        mock.assert_called_once_with(job_id, "default")
+        assert response.status_code == 200
 
-    def test_not_found(self, client: TestClient, mocker: MockFixture) -> None:
+        metadata = WorkloadMetadata.model_validate_json(response.text)
+        assert metadata.managed_resource_id == wl.owner_uid
+        assert metadata.execution_status == wl.execution_status
+        assert metadata.kueue_status.conditions == wl.status.conditions
+        assert metadata.kueue_status == wl.status
+
+    def test_workload_not_found(self, client: TestClient, mocker: MockFixture) -> None:
         mock = mocker.patch.object(
             KubernetesService, "workload_for_managed_resource", return_value=None
         )
@@ -85,6 +118,26 @@ class TestJobStatus:
 
         assert response.status_code == 404
         mock.assert_called_once_with(job_id, "default")
+
+    def test_workload_metadata_not_found(
+        self, client: TestClient, mocker: MockFixture
+    ) -> None:
+        workload_mock = mocker.patch.object(
+            KubernetesService,
+            "workload_for_managed_resource",
+            return_value=mocker.Mock(KueueWorkload),
+        )
+        metadata_mock = mocker.patch.object(
+            WorkloadMetadata,
+            "from_managed_workload",
+            side_effect=ValueError("Workload not found"),
+        )
+
+        job_id = uuid.uuid4()
+        response = client.get(f"/jobs/{job_id}/status")
+
+        assert response.status_code == 404
+        metadata_mock.assert_called_once_with(workload_mock.return_value)
 
 
 class TestJobLogs:
