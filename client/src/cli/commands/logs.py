8f9a6a8fafe321e660690f8e88f613c59501823f
Adrian Rumpold <a.rumpold@gmail.com>
2024-09-03 08:58:39 +0200

    fix(client): Use generated API client for log streaming

--- client/src/cli/commands/logs.py
+++ client/src/cli/commands/logs.py
@@ -1,23 +1,30 @@
 import argparse
-import asyncio
-from collections.abc import AsyncGenerator
 from enum import Enum
-from pprint import pp
 from typing import Any
-from urllib.parse import urljoin
-
-import aiohttp
 
 import openapi_client
 
-from .util import _make_api_client, handle_api_exception, with_job_mgmt_api
+from .util import with_job_mgmt_api
 
 
 class LogCommands(Enum):
     NAMESPACE = "namespace"
     UID = "uid"
     TAIL = "tail"
-    STREAM = "stream"
+    FOLLOW = "follow"
+
+    def to_argparse(self) -> list[str]:
+        if self.short_command:
+            return [f"-{self.short_command}", f"--{self.value}"]
+        else:
+            return [f"--{self.value}"]
+
+    @property
+    def short_command(self) -> str | None:
+        _short_commands = {
+            "follow": "f",
+        }
+        return _short_commands.get(self.value, None)
 
     @classmethod
     def values(cls) -> list[str]:
@@ -25,8 +32,12 @@ class LogCommands(Enum):
 
 
 def sanitize_log_params(args: argparse.Namespace) -> dict[str, Any]:
+    _param_map = {
+        LogCommands.FOLLOW.value: "stream",
+    }
     return {
-        k: v
+        # translate argparse args to openapi_client params
+        _param_map.get(k, k): v
         for k, v in vars(args).items()
         if v is not None and k in LogCommands.values()
     }
@@ -40,40 +51,22 @@ def stringify_dict(d: dict[str, Any]) -> dict[str, str]:
 def logs(client: openapi_client.JobManagementApi, args: argparse.Namespace) -> None:
     params = sanitize_log_params(args)
     resp = client.logs_jobs_uid_logs_get(**params)
-    pp(resp)
+    print(resp)
 
 
-async def async_generate_log_lines(
-    client: openapi_client.JobManagementApi, params: dict
-) -> AsyncGenerator[str, None]:
-    url = urljoin(client.api_client.configuration.host, f"jobs/{params['uid']}/logs")
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=stringify_dict(params)) as response:
-            async for line in response.content.iter_any():
-                yield line.decode().strip()
-
-
-async def print_streamed_log_lines(
-    client: openapi_client.JobManagementApi, params: dict
+@with_job_mgmt_api
+def stream_logs(
+    client: openapi_client.JobManagementApi, args: argparse.Namespace
 ) -> None:
-    try:
-        async for log_line in async_generate_log_lines(client, params):
-            pp(log_line)
-    except aiohttp.ClientResponseError as e:
-        handle_api_exception(e, "log stream")
-        raise
-
-
-async def async_stream_logs(args: argparse.Namespace) -> None:
     params = sanitize_log_params(args)
-    with _make_api_client() as api:
-        client = openapi_client.JobManagementApi(api)
-        await print_streamed_log_lines(client, params)
+    resp = client.logs_jobs_uid_logs_get_without_preload_content(**params)
+    for line in resp.stream():
+        print(line.decode("utf-8"), end="")
 
 
 def handle_logs_cmd(args: argparse.Namespace) -> None:
-    if args.stream:
-        asyncio.run(async_stream_logs(args))
+    if args.follow:
+        stream_logs(args)
     else:
         logs(args)
 
@@ -82,18 +75,18 @@ def add_parser(subparsers: Any, parent: argparse.ArgumentParser) -> None:
     # jobby logs, command to fetch logs for workload
     parser = subparsers.add_parser("logs", description="Get logs for specified job.")
     parser.add_argument(
-        f"--{LogCommands.NAMESPACE.value}",
+        *LogCommands.NAMESPACE.to_argparse(),
         help="Kubernetes namespace the job was created in, "
         "defaults to currently active namespace.",
     )
     parser.add_argument(LogCommands.UID.value, metavar="<ID>")
     parser.add_argument(
-        f"--{LogCommands.STREAM.value}",
+        *LogCommands.FOLLOW.to_argparse(),
         action="store_true",
         help="Whether to stream logs",
     )
     parser.add_argument(
-        f"--{LogCommands.TAIL.value}",
+        *LogCommands.TAIL.to_argparse(),
         type=int,
         help="Lines of recent logs to display",
     )
