d317d64f72ba288e57d02637b7d6ace6041c75fd
Adrian Rumpold <a.rumpold@gmail.com>
2024-09-24 15:48:16 +0200

    Support for CLI configuration (#103)
    * feat(cli): Support for CLI configuration
    
    Configuration is parsed using pydantic-settings from the following
    sources (in order of precedence):
    
    - Environment variables
    - CLI arguments
    - `pyproject.toml` file (`tool.jobq` table)
    
    ---------
    
    Co-authored-by: Nicholas Junge <n.junge@appliedai-institute.de>

--- client/src/cli/commands/status.py
+++ client/src/cli/commands/status.py
@@ -3,23 +3,29 @@ from argparse import ArgumentParser
 from typing import Any
 
 import openapi_client
+from cli.types import Settings
+from cli.util import with_job_mgmt_api
 from jobq.utils.helpers import format_dict
-
-from .util import with_job_mgmt_api
 
 
 @with_job_mgmt_api
-def status(client: openapi_client.JobManagementApi, args: argparse.Namespace) -> None:
+def status(
+    client: openapi_client.JobManagementApi,
+    args: argparse.Namespace,
+    settings: Settings,
+) -> None:
     resp = client.status_jobs_uid_status_get(uid=args.uid)
     print(format_dict(resp.to_dict()))
 
 
 def add_parser(subparsers: Any, parent: ArgumentParser) -> None:
     # jobq status, the status querying command
+    help = "Query the status of a previously submitted job"
     parser: argparse.ArgumentParser = subparsers.add_parser(
         "status",
         parents=[parent],
-        description="Query the status of a previously dispatched job.",
+        help=help,
+        description=help,
     )
 
     # unique identifier of the job
