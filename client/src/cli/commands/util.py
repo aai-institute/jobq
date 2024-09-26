fc03aee24584220f792dad5a7ba3b9b1b635967d
Max Mynter <maxmynter@me.com>
2024-09-26 15:48:13 +0200

    Revert "Add Config CLI parser"
    In favour of another PR implementing config handling.
    
    This reverts commit 8935a4442095cadd49574875060006f443de11d1.

--- client/src/cli/commands/util.py
+++ client/src/cli/commands/util.py
@@ -1,68 +1,19 @@
-from __future__ import annotations
-
 from collections.abc import Callable
 from functools import wraps
-from pathlib import Path
-from typing import Any, Concatenate, ParamSpec, TypeVar, cast
-
-import yaml
+from typing import Concatenate, ParamSpec, TypeVar
 
 import openapi_client
 from openapi_client.exceptions import ApiException
 
-DEFAULT_CONFIG = {"backend": {"host": "http://localhost:8000"}}
-CONFIG_PATH = Path.home() / ".config" / "jobq" / "config.yaml"
-
-
-C = TypeVar("C", bound="Config")
-
-
-class Config:
-    _instance: Config | None = None
-    data: dict[str, Any]
-
-    def __new__(cls: type[C]) -> C:
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance.data = cls._instance.load()
-        return cast(C, cls._instance)
-
-    def load(self) -> dict[str, Any]:
-        if CONFIG_PATH.exists():
-            return yaml.safe_load(CONFIG_PATH.read_text())
-        return DEFAULT_CONFIG.copy()
-
-    def save(self) -> None:
-        CONFIG_PATH.parent.mkdir(parents=True, exist_ok=True)
-        CONFIG_PATH.write_text(yaml.dump(self.data))
-
-    def get(self, key: str) -> Any:
-        keys = key.split(".")
-        value = self.data
-        for k in keys:
-            if isinstance(value, dict) and k in value:
-                value = value[k]
-            else:
-                raise ValueError(f"Didnot find {key} in config")
-        return value
-
-    def set(self, key: str, value: str) -> None:
-        keys = key.split(".")
-        data = self.data
-        for k in keys[:-1]:
-            if k not in data:
-                data[k] = {}
-            data = data[k]
-        data[keys[-1]] = value
-        self.save()
-
+# TODO: Factor backend host url into some kind of config file
+BACKEND_HOST = "http://localhost:8000"
 
 T = TypeVar("T")
 P = ParamSpec("P")
 
 
 def _make_api_client() -> openapi_client.ApiClient:
-    api_config = openapi_client.Configuration(host=Config().get("backend.host"))
+    api_config = openapi_client.Configuration(host=BACKEND_HOST)
     return openapi_client.ApiClient(api_config)
 
 
