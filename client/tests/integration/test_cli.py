dc41ce289e8286af17b0d51313ce2620ff590ace
Adrian Rumpold <a.rumpold@gmail.com>
2024-09-27 13:00:46 +0200

    fix(ci): Capture stdout in pytest

--- client/tests/integration/test_cli.py
+++ client/tests/integration/test_cli.py
@@ -15,5 +15,9 @@ def test_help(runner: CliRunner, command: str, flag: str) -> None:
     """Test that the help message is displayed correctly for all commands."""
 
     result = runner.invoke(cli, [command, flag] if command else [flag])
+
+    if result.exit_code != 0:
+        print(result.output)  # aid debugging in CI
+
     assert result.exit_code == 0
     assert "usage" in result.output
