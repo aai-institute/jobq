905139180c29d95aff98dc354016f97c26b75024
Nicholas Junge <n.junge@appliedai-institute.de>
2024-10-01 08:44:14 +0200

    Move docs to top-level directory
    This makes the docs a top-level directory in the hierarchy rather than
    hiding it in a subfolder of the client.
    
    It's preferable this way since the docs also contain some information
    regarding the backend, and we are not (currently) planning on making a
    separate backend docs folder.

--- docs/_scripts/gen_api_ref_pages.py
+++ docs/_scripts/gen_api_ref_pages.py
@@ -14,9 +14,9 @@ import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("src").rglob("*.py")):
-    module_path = path.relative_to("src").with_suffix("")
-    doc_path = path.relative_to("src").with_suffix(".md")
+for path in sorted(Path("client/src").rglob("*.py")):
+    module_path = path.relative_to("client/src").with_suffix("")
+    doc_path = path.relative_to("client/src").with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
     parts = list(module_path.parts)
